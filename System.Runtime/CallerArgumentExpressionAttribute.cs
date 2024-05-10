533c9b2b37b6fbfd9d03a5b91306a323aabb5dcc
Cory Charlton <ccharlton@pobox.com>
2024-05-10 12:46:58 -0700

    Adding CallerArgumentExpressionAttribute and CallerMemberNameAttribute

---
+++ System.Runtime/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+
+        public string ParameterName { get; }
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
---
+++ System.Runtime/CallerMemberNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public sealed class CallerMemberNameAttribute : Attribute
+    {
+        public CallerMemberNameAttribute()
+        {
+        }
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
